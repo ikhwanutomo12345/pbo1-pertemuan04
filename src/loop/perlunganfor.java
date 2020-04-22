405a3ea4dd96d831556c4f5549bbcc815dd8e68d
ikhwanutomo12345 <ikhwanutomo12345@gmail.com>
2020-04-22 09:29:30 +0700

    perulangan for

---
+++ src/loop/perlunganfor.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class perlunganfor {
+    int isiGelas = 0;
+    int penuh = 225;
+
+    System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+    System.out.println("Isi Gelas Ketika penuh : " + penuh + "ml");
+
+    for(isiGelas = 0 ; isiGelas <= penuh ; isiGelas++){
+        System.out.println("Sedang mengisi gelas..");
+        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+    }
+
+    System.out.println("Finale : Isi Gelas Sekarang : " + isiGelas + "ml");
+}
