8cd848e0e7beea0b62a114ca1f50df5436015013
ikhwanutomo12345 <ikhwanutomo12345@gmail.com>
2020-04-22 09:45:46 +0700

    array

--- src/loop/perlunganfor.java
+++ src/loop/perlunganfor.java
@@ -1,16 +1,18 @@
 package loop;
 
 public class perlunganfor {
-    int isiGelas = 0;
-    int penuh = 225;
+    public static void main(String[] args) {
+        int isiGelas = 0;
+        int penuh = 225;
 
-    System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
-    System.out.println("Isi Gelas Ketika penuh : " + penuh + "ml");
-
-    for(isiGelas = 0 ; isiGelas <= penuh ; isiGelas++){
-        System.out.println("Sedang mengisi gelas..");
         System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
-    }
+        System.out.println("Isi Gelas Ketika penuh : " + penuh + "ml");
 
-    System.out.println("Finale : Isi Gelas Sekarang : " + isiGelas + "ml");
+        for (isiGelas = 0; isiGelas <= penuh; isiGelas++) {
+            System.out.println("Sedang mengisi gelas..");
+            System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        }
+
+        System.out.println("Finale : Isi Gelas Sekarang : " + isiGelas + "ml");
+    }
 }
